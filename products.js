4907969ecc9f7d449677b452f58354f16fa47057
Palak Agarwal <palakagarwal301@gmail.com>
2026-04-23 03:27:29 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,19 @@
+// products.js
+const products = [
+  {id:1,cat:"booties",name:"Pink Basket Booties",price:450,desc:"Soft handmade crochet booties.",imgs:["booties_pink_basket.jpg"]},
+  {id:2,cat:"booties",name:"Lace Wood Booties",price:500,desc:"Elegant lace crochet booties.",imgs:["booties_lace_wood.jpg"]},
+  {id:3,cat:"bags",name:"Rose Drawstring Bag",price:950,desc:"Crochet drawstring bag with rose motif.",imgs:["bag_rose_drawstring.jpg"]},
+  {id:4,cat:"bags",name:"Sunflower Sling Bag",price:1200,desc:"Crochet sling bag with sunflower design.",imgs:["bag_sunflower_sling.jpg"]},
+  {id:5,cat:"plushies",name:"Custom Plushie",price:1500,desc:"Handmade plushie customized to your request.",imgs:["custom_plushie.jpg"]},
+  {id:6,cat:"plushies",name:"Pooh Plushie",price:1400,desc:"Crochet plushie inspired by Pooh.",imgs:["pooh.png"]},
+  {id:7,cat:"plushies",name:"Shark Plushie",price:1400,desc:"Crochet shark plushie.",imgs:["shark_plushie.png"]},
+  {id:8,cat:"plushies",name:"Santa Plushie",price:1400,desc:"Festive crochet Santa plushie.",imgs:["santa_plushie.jpg"]},
+  {id:9,cat:"coasters",name:"Leaf Heart Coaster",price:250,desc:"Crochet coaster shaped like a leaf heart.",imgs:["coaster_leaf_heart.jpg"]},
+  {id:10,cat:"coasters",name:"Coasters & Mats Set",price:600,desc:"Handmade crochet coasters and mats.",imgs:["coasters_mats.png"]},
+  {id:11,cat:"flowers",name:"Sunflower",price:300,desc:"Crochet sunflower decoration.",imgs:["sunflower.png"]},
+  {id:12,cat:"flowers",name:"Daisy Flower",price:300,desc:"Crochet daisy flower decoration.",imgs:["daisy_flower.png"]},
+  {id:13,cat:"accessories",name:"Bow Headband",price:350,desc:"Crochet headband with bow.",imgs:["bow_headband.png"]},
+  {id:14,cat:"rakhis",name:"Rakhi Trio",price:400,desc:"Set of three handmade crochet rakhis.",imgs:["rakhis_trio.jpg"]},
+  {id:15,cat:"mandala",name:"Mandala Doily",price:700,desc:"Crochet mandala doily centerpiece.",imgs:["mandala_doily.jpg"]},
+  {id:16,cat:"mandala",name:"Mandala Centre Mat",price:800,desc:"Crochet mandala centre mat.",imgs:["mandala_centre_mat.jpg"]}
+];
