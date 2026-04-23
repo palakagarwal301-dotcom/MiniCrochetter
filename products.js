5ec829e27f5155722d90b4f16655ce4faae79ced
Palak Agarwal <palakagarwal301@gmail.com>
2026-04-23 17:40:05 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,39 +1,39 @@
 const products = [
 
-  // 🌼 Flowers
+  // 🌼 ENTRY PRODUCTS (conversion drivers)
   {id:1, name:"Daisy Flower", price:349, category:"flower", img:"daisy.png"},
   {id:2, name:"Sunflower", price:399, category:"flower", img:"sunflower.png"},
   {id:3, name:"Daisy Variant", price:349, category:"flower", img:"daisy_flower.png"},
   {id:4, name:"Sunflower Variant", price:399, category:"flower", img:"sunflower_flower.png"},
 
-  // 🎀 Accessories
+  // 🎀 SMALL ADD-ONS (upsell)
   {id:5, name:"Bow Headband", price:199, category:"accessory", img:"bow_headband.png"},
   {id:6, name:"Rakhi Bow", price:149, category:"accessory", img:"rakhi_bow.jpg"},
   {id:7, name:"Rakhi Set", price:299, category:"accessory", img:"rakhis_trio.jpg"},
 
-  // 🧸 Plushies
-  {id:8, name:"Mini Plushies", price:599, category:"plushie", img:"mini_plushies.png"},
-  {id:9, name:"Pooh Plushie", price:899, category:"plushie", img:"pooh.png"},
-  {id:10, name:"Minion Plushie", price:899, category:"plushie", img:"minion.png"},
-  {id:11, name:"Shark Plushie", price:899, category:"plushie", img:"shark_plushie.png"},
-  {id:12, name:"Santa Plushie", price:899, category:"plushie", img:"santa_plushie.jpg"},
+  // 🧸 CORE PRODUCTS (profit drivers)
+  {id:8, name:"Mini Plushies", price:699, category:"plushie", img:"mini_plushies.png"},
+  {id:9, name:"Pooh Plushie", price:1099, category:"plushie", img:"pooh.png"},
+  {id:10, name:"Minion Plushie", price:1099, category:"plushie", img:"minion.png"},
+  {id:11, name:"Shark Plushie", price:1099, category:"plushie", img:"shark_plushie.png"},
+  {id:12, name:"Santa Plushie", price:1099, category:"plushie", img:"santa_plushie.jpg"},
 
-  // 🧶 Decor
-  {id:13, name:"Mandala Centre Mat", price:799, category:"decor", img:"mandala_centre_mat.jpg"},
-  {id:14, name:"Mandala Doily", price:699, category:"decor", img:"mandala_doily.jpg"},
-  {id:15, name:"Coasters Set", price:349, category:"decor", img:"coasters_mats.png"},
-  {id:16, name:"Leaf Coaster", price:199, category:"decor", img:"coaster_leaf_heart.jpg"},
-  {id:17, name:"Sunflower Coaster", price:199, category:"decor", img:"sunflower-coaster.png"},
+  // 🧶 DECOR (balanced margin)
+  {id:13, name:"Mandala Centre Mat", price:899, category:"decor", img:"mandala_centre_mat.jpg"},
+  {id:14, name:"Mandala Doily", price:799, category:"decor", img:"mandala_doily.jpg"},
+  {id:15, name:"Coasters Set", price:399, category:"decor", img:"coasters_mats.png"},
+  {id:16, name:"Leaf Coaster", price:249, category:"decor", img:"coaster_leaf_heart.jpg"},
+  {id:17, name:"Sunflower Coaster", price:249, category:"decor", img:"sunflower-coaster.png"},
 
-  // 👜 Bags
-  {id:18, name:"Rose Drawstring Bag", price:1199, category:"bags", img:"bag_rose_drawstring.jpg"},
-  {id:19, name:"Sunflower Sling Bag", price:1399, category:"bags", img:"bag_sunflower_sling.jpg"},
+  // 👜 HIGH VALUE
+  {id:18, name:"Rose Drawstring Bag", price:1499, category:"bags", img:"bag_rose_drawstring.jpg"},
+  {id:19, name:"Sunflower Sling Bag", price:1699, category:"bags", img:"bag_sunflower_sling.jpg"},
 
-  // 👶 Baby
-  {id:20, name:"Baby Booties Lace", price:349, category:"baby", img:"booties_lace_wood.jpg"},
-  {id:21, name:"Pink Booties", price:349, category:"baby", img:"booties_pink_basket.jpg"},
+  // 👶 BABY
+  {id:20, name:"Baby Booties Lace", price:399, category:"baby", img:"booties_lace_wood.jpg"},
+  {id:21, name:"Pink Booties", price:399, category:"baby", img:"booties_pink_basket.jpg"},
 
-  // 🎁 Custom
-  {id:22, name:"Custom Plushie", price:1299, category:"custom", img:"custom_plushie.jpg"}
+  // 🎁 PREMIUM
+  {id:22, name:"Custom Plushie", price:1599, category:"custom", img:"custom_plushie.jpg"}
 
 ];
