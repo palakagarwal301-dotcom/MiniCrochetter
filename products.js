6b12390531d126cd4459e96be790649cfe4d8197
Palak Agarwal <palakagarwal301@gmail.com>
2026-04-23 17:51:35 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,39 +1,18 @@
 const products = [
 
-  // 🌼 Flowers
-  {id:1, name:"Daisy Flower", price:349, category:"flower", img:"daisy.png", desc:"✨ Handmade crochet flower. Perfect for gifting"},
-  {id:2, name:"Sunflower", price:399, category:"flower", img:"sunflower.png", desc:"🌻 Bright handmade sunflower for decor & gifts"},
-  {id:3, name:"Daisy Variant", price:349, category:"flower", img:"daisy_flower.png", desc:"✨ Soft handmade floral design"},
-  {id:4, name:"Sunflower Variant", price:399, category:"flower", img:"sunflower_flower.png", desc:"🌻 Premium handmade crochet flower"},
+  {id:1, name:"Daisy Flower", price:349, category:"flower", img:"daisy.png", desc:"✨ Handmade crochet flower"},
+  {id:2, name:"Sunflower", price:399, category:"flower", img:"sunflower.png", desc:"🌻 Bright handmade sunflower"},
 
-  // 🎀 Accessories
+  {id:3, name:"Pooh Plushie", price:1099, category:"plushie", img:"pooh.png", desc:"🧸 Soft handmade plushie"},
+  {id:4, name:"Minion Plushie", price:1099, category:"plushie", img:"minion.png", desc:"🧸 Fun crochet character"},
+
   {id:5, name:"Bow Headband", price:199, category:"accessory", img:"bow_headband.png", desc:"🎀 Cute handmade headband"},
-  {id:6, name:"Rakhi Bow", price:149, category:"accessory", img:"rakhi_bow.jpg", desc:"🎁 Handmade rakhi for special occasions"},
-  {id:7, name:"Rakhi Set", price:299, category:"accessory", img:"rakhis_trio.jpg", desc:"🎁 Set of handmade rakhis"},
+  {id:6, name:"Rakhi Set", price:299, category:"accessory", img:"rakhis_trio.jpg", desc:"🎁 Handmade rakhi set"},
 
-  // 🧸 Plushies
-  {id:8, name:"Mini Plushies", price:699, category:"plushie", img:"mini_plushies.png", desc:"🧸 Cute mini plushies, handmade"},
-  {id:9, name:"Pooh Plushie", price:1099, category:"plushie", img:"pooh.png", desc:"🧸 Soft handmade plushie, made to order"},
-  {id:10, name:"Minion Plushie", price:1099, category:"plushie", img:"minion.png", desc:"🧸 Fun handmade character plushie"},
-  {id:11, name:"Shark Plushie", price:1099, category:"plushie", img:"shark_plushie.png", desc:"🧸 Unique handmade plush toy"},
-  {id:12, name:"Santa Plushie", price:1099, category:"plushie", img:"santa_plushie.jpg", desc:"🎄 Festive handmade plushie"},
+  {id:7, name:"Mandala Mat", price:899, category:"decor", img:"mandala_centre_mat.jpg", desc:"🧶 Crochet home decor"},
+  {id:8, name:"Coasters Set", price:399, category:"decor", img:"coasters_mats.png", desc:"☕ Handmade coasters"},
 
-  // 🧶 Decor
-  {id:13, name:"Mandala Centre Mat", price:899, category:"decor", img:"mandala_centre_mat.jpg", desc:"🧶 Handmade mandala decor"},
-  {id:14, name:"Mandala Doily", price:799, category:"decor", img:"mandala_doily.jpg", desc:"🧶 Elegant crochet doily"},
-  {id:15, name:"Coasters Set", price:399, category:"decor", img:"coasters_mats.png", desc:"☕ Set of handmade coasters"},
-  {id:16, name:"Leaf Coaster", price:249, category:"decor", img:"coaster_leaf_heart.jpg", desc:"🍃 Leaf design coaster"},
-  {id:17, name:"Sunflower Coaster", price:249, category:"decor", img:"sunflower-coaster.png", desc:"🌻 Handmade coaster"},
-
-  // 👜 Bags
-  {id:18, name:"Rose Drawstring Bag", price:1499, category:"bags", img:"bag_rose_drawstring.jpg", desc:"👜 Handmade stylish bag"},
-  {id:19, name:"Sunflower Sling Bag", price:1699, category:"bags", img:"bag_sunflower_sling.jpg", desc:"👜 Premium crochet sling bag"},
-
-  // 👶 Baby
-  {id:20, name:"Baby Booties Lace", price:399, category:"baby", img:"booties_lace_wood.jpg", desc:"👶 Soft handmade baby booties"},
-  {id:21, name:"Pink Booties", price:399, category:"baby", img:"booties_pink_basket.jpg", desc:"👶 Cute crochet baby shoes"},
-
-  // 🎁 Custom
-  {id:22, name:"Custom Plushie", price:1599, category:"custom", img:"custom_plushie.jpg", desc:"🎁 Fully customized handmade plushie"}
+  {id:9, name:"Rose Bag", price:1499, category:"bags", img:"bag_rose_drawstring.jpg", desc:"👜 Stylish crochet bag"},
+  {id:10, name:"Baby Booties", price:399, category:"baby", img:"booties_pink_basket.jpg", desc:"👶 Soft baby shoes"}
 
 ];
