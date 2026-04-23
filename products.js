c2c8f6cd196f99f836bd24a009fc125975f11b88
Palak Agarwal <palakagarwal301@gmail.com>
2026-04-24 03:39:12 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,7 +1,7 @@
 const products = [
-  { id:1, name:"Pink Booties", price:649, img:"images/booties1.jpg" },
-  { id:2, name:"Classic Booties", price:549, img:"images/booties2.jpg" },
-  { id:3, name:"Winnie Plushie", price:1499, img:"images/pooh.jpg" },
-  { id:4, name:"Minion Plushie", price:1299, img:"images/minion.jpg" },
-  { id:5, name:"Sunflower Coaster", price:349, img:"images/sunflower.jpg" }
+  {id:1,name:"Pink Booties",price:649,cat:"booties",img:"booties1.jpg"},
+  {id:2,name:"Classic Booties",price:549,cat:"booties",img:"booties2.jpg"},
+  {id:3,name:"Winnie Plushie",price:1499,cat:"plushies",img:"pooh.jpg"},
+  {id:4,name:"Minion Plushie",price:1299,cat:"plushies",img:"minion.jpg"},
+  {id:5,name:"Sunflower Coaster",price:349,cat:"coasters",img:"sunflower.jpg"}
 ];
