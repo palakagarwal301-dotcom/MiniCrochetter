2317dcbff268a6fbbbf8130afce0d10558e722c6
Palak Agarwal <palakagarwal301@gmail.com>
2026-04-24 03:25:25 +0530

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,56 +1,37 @@
-let cart = [];
-
-function render(){
-  const box = document.getElementById("products");
-  box.innerHTML = "";
-
-  if(!products || products.length === 0){
-    box.innerHTML = "<p>No products found</p>";
-    return;
+const products = [
+  {
+    id: 1,
+    name: "Pink Booties",
+    price: 649,
+    cat: "booties",
+    img: "images/booties1.jpg"
+  },
+  {
+    id: 2,
+    name: "Classic Booties",
+    price: 549,
+    cat: "booties",
+    img: "images/booties2.jpg"
+  },
+  {
+    id: 3,
+    name: "Winnie Plushie",
+    price: 1499,
+    cat: "plushies",
+    img: "images/pooh.jpg"
+  },
+  {
+    id: 4,
+    name: "Minion Plushie",
+    price: 1299,
+    cat: "plushies",
+    img: "images/minion.jpg"
+  },
+  {
+    id: 5,
+    name: "Sunflower Coaster",
+    price: 349,
+    cat: "coasters",
+    img: "images/sunflower.jpg"
   }
-
-  products.forEach(p=>{
-    const div = document.createElement("div");
-    div.className = "card";
-
-    div.innerHTML = `
-      <img src="${p.img}" onerror="this.src='https://via.placeholder.com/300'">
-      <h4>${p.name}</h4>
-      <p>₹${p.price}</p>
-      <button>Add to Cart</button>
-    `;
-
-    div.querySelector("button").onclick = () => {
-      cart.push(p);
-      updateTotal();
-    };
-
-    box.appendChild(div);
-  });
-}
-
-function updateTotal(){
-  let total = cart.reduce((s,i)=>s+i.price,0);
-  document.getElementById("total").innerText = total;
-}
-
-function checkout(){
-  if(cart.length === 0){
-    alert("Cart is empty");
-    return;
-  }
-
-  let msg = "🧶 Order:%0A%0A";
-
-  cart.forEach(i=>{
-    msg += i.name + " - ₹" + i.price + "%0A";
-  });
-
-  let total = cart.reduce((s,i)=>s+i.price,0);
-  msg += "%0ATotal: ₹" + total;
-
-  window.open("https://wa.me/918886122232?text=" + msg);
-}
-
-render();
-updateTotal();
+];
