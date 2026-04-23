1cdcd851c1491ae442717f0f337d3cb5e059c7f5
Palak Agarwal <palakagarwal301@gmail.com>
2026-04-23 20:50:22 +0530

    Create components.js

---
+++ components.js
@@ -0,0 +1,13 @@
+function loadNavbar(){
+  return `
+  <nav class="navbar">
+    <div class="logo">Mini Crochetter 🧶</div>
+
+    <div class="links">
+      <a href="index.html">Home</a>
+      <a href="shop.html">Shop</a>
+      <a href="cart.html">Cart 🛒 (<span id="cartCount">0</span>)</a>
+    </div>
+  </nav>
+  `;
+}
