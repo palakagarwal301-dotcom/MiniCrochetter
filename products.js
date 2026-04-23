5b7d645e7d44e1f475d898a103b82421d876a948
Palak Agarwal <palakagarwal301@gmail.com>
2026-04-23 22:00:46 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,18 +1,5 @@
 const products = [
-
-  {id:1, name:"Daisy Flower", price:349, category:"flower", img:"daisy.png", desc:"✨ Handmade crochet flower"},
-  {id:2, name:"Sunflower", price:399, category:"flower", img:"sunflower.png", desc:"🌻 Bright handmade sunflower"},
-
-  {id:3, name:"Pooh Plushie", price:1099, category:"plushie", img:"pooh.png", desc:"🧸 Soft handmade plushie"},
-  {id:4, name:"Minion Plushie", price:1099, category:"plushie", img:"minion.png", desc:"🧸 Fun crochet character"},
-
-  {id:5, name:"Bow Headband", price:199, category:"accessory", img:"bow_headband.png", desc:"🎀 Cute handmade headband"},
-  {id:6, name:"Rakhi Set", price:299, category:"accessory", img:"rakhis_trio.jpg", desc:"🎁 Handmade rakhi set"},
-
-  {id:7, name:"Mandala Mat", price:899, category:"decor", img:"mandala_centre_mat.jpg", desc:"🧶 Crochet home decor"},
-  {id:8, name:"Coasters Set", price:399, category:"decor", img:"coasters_mats.png", desc:"☕ Handmade coasters"},
-
-  {id:9, name:"Rose Bag", price:1499, category:"bags", img:"bag_rose_drawstring.jpg", desc:"👜 Stylish crochet bag"},
-  {id:10, name:"Baby Booties", price:399, category:"baby", img:"booties_pink_basket.jpg", desc:"👶 Soft baby shoes"}
-
+  {name:"Flower Keychain", price:199, img:"img1.jpg"},
+  {name:"Crochet Bear", price:499, img:"img2.jpg"},
+  {name:"Mini Bouquet", price:299, img:"img3.jpg"}
 ];
