79ecc39e45f109b79a6556789c13e266532b8af7
Palak Agarwal <palakagarwal301@gmail.com>
2026-04-24 11:23:45 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,10 @@
+const products = [
+  {
+    id: 1,
+    cat: "booties",
+    name: "Pink Ruffle Booties",
+    price: 649,
+    imgs: ["booties_ruffle.jpg"],
+    desc: "Soft handmade baby booties with bow."
+  }
+];
