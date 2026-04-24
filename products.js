33f4fb06a35186295954d6430684b2d9be2a5736
Palak Agarwal <palakagarwal301@gmail.com>
2026-04-24 10:37:24 +0530

    Delete products.js

--- products.js
+++
@@ -1,7 +0,0 @@
-const products = [
-  {id:1,name:"Pink Booties",price:649,cat:"booties",img:"booties1.jpg"},
-  {id:2,name:"Classic Booties",price:549,cat:"booties",img:"booties2.jpg"},
-  {id:3,name:"Winnie Plushie",price:1499,cat:"plushies",img:"pooh.jpg"},
-  {id:4,name:"Minion Plushie",price:1299,cat:"plushies",img:"minion.jpg"},
-  {id:5,name:"Sunflower Coaster",price:349,cat:"coasters",img:"sunflower.jpg"}
-];
