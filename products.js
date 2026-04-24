4a4e78c8245b5545154b8198cab363d39fee57fc
Palak Agarwal <palakagarwal301@gmail.com>
2026-04-24 11:59:07 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,10 +1,26 @@
-const products = [
+window.PRODUCTS = [
   {
     id: 1,
     cat: "booties",
     name: "Pink Ruffle Booties",
     price: 649,
     imgs: ["booties_ruffle.jpg"],
-    desc: "Soft handmade baby booties with bow."
+    desc: "Soft handmade booties with bow detail"
+  },
+  {
+    id: 2,
+    cat: "plushies",
+    name: "Winnie the Pooh Plushie",
+    price: 1499,
+    imgs: ["pooh.png"],
+    desc: "Classic handmade Pooh plushie"
+  },
+  {
+    id: 3,
+    cat: "bags",
+    name: "Rose Crochet Bag",
+    price: 999,
+    imgs: ["bag_rose.jpg"],
+    desc: "Elegant handmade crochet sling bag"
   }
 ];
